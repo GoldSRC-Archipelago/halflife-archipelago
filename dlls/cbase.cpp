3702d6d099cefdaa26c139aae99830596b076bef
ObsidainPlague <42327589+ObsidianPlague@users.noreply.github.com>
2023-10-28 10:30:58 -0500

    add new 'dropped' property for weapons

--- dlls/cbase.cpp
+++ dlls/cbase.cpp
@@ -795,7 +795,7 @@ int CBaseEntity::DamageDecal(int bitsDamageType)
 
 // NOTE: szName must be a pointer to constant memory, e.g. "monster_class" because the entity
 // will keep a pointer to it after this call.
-CBaseEntity* CBaseEntity::Create(const char* szName, const Vector& vecOrigin, const Vector& vecAngles, edict_t* pentOwner)
+CBaseEntity* CBaseEntity::Create(const char* szName, const Vector& vecOrigin, const Vector& vecAngles, edict_t* pentOwner, bool bDropped)
 {
 	edict_t* pent;
 	CBaseEntity* pEntity;
@@ -810,6 +810,7 @@ CBaseEntity* CBaseEntity::Create(const char* szName, const Vector& vecOrigin, co
 	pEntity->pev->owner = pentOwner;
 	pEntity->pev->origin = vecOrigin;
 	pEntity->pev->angles = vecAngles;
+	pEntity->pev->iuser4 = int(bDropped);
 	DispatchSpawn(pEntity->edict());
 	return pEntity;
 }
